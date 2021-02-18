9dcd3192e3e8a60afdafe671268756db7446d601
Yajie Yu <409849275@qq.com>
2021-02-18 20:00:17 +0800

    add outside repo

--- cmake-build-debug/devel/share/nlink_parser/cmake/nlink_parser-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(nlink_parser_MSG_INCLUDE_DIRS "/home/blasi/Desktop/nlink_parser/msg")
-set(nlink_parser_MSG_DEPENDENCIES std_msgs)
